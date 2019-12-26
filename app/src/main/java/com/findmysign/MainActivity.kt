8398207b7c52287cf55cf90548be5a9c1bf4363c
michael-hunting <michael.hunting@coxautoinc.com>
2019-12-26 16:29:13 -0700

    added exf reading

--- app/src/main/java/com/findmysign/MainActivity.kt
+++ app/src/main/java/com/findmysign/MainActivity.kt
@@ -19,55 +19,61 @@ class MainActivity : AppCompatActivity() {
     val PERMISSION_CODE = 1000
     private val IMAGE_CAPTURE_CODE = 1001
     var imageuri: Uri? = null
-    var yourmamma: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        picture_btn.setOnClickListener{
+        picture_btn.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
-                    checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+                ) {
 
-                    val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    val permission = arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
                     requestPermissions(permission, PERMISSION_CODE)
-               }
-
-                else{
+                } else {
                     openCamera()
                 }
 
-                }
-            else{
+            } else {
                 openCamera()
             }
 
-            }
         }
-
-
+    }
 
     private fun openCamera() {
         val values = ContentValues()
         values.put(MediaStore.Images.Media.TITLE, "New Picture")
         values.put(MediaStore.Images.Media.DESCRIPTION, "Taken From The Camera")
-        imageuri =  contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        imageuri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
 
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageuri)
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<out String>,grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when(requestCode){
-            PERMISSION_CODE ->{if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED&&grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                openCamera()
-            }
-                else{
-                Toast.makeText(this,"This app requires permission to function.", Toast.LENGTH_SHORT).show()
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "This app requires permission to function.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
@@ -76,6 +82,19 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK) {
             image_view.setImageURI(imageuri)
+            //exif_txt.text =
+            try {
+                if (imageuri != null) {
+                    val uri = imageuri!!
+                    val stream = contentResolver.openInputStream(uri)
+                    val exif = ExifInterface(stream!!)
+                    exif_txt.text = exif.latLong!![0].toString()
+                }
+            } catch (ex: Exception) {
+                exif_txt.text = ex.message
+            }
+
+            //exif_txt.text = exif.toString()
 
 
         }
